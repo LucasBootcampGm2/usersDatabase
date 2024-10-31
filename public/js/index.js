b3c5a6753bd9c57c79e81b6c014a71e10ed71d4a
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-10-31 15:09:38 -0300

    feat(index.js): connect express server with express router and its middlewares

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,14 @@
 import express from "express";
-
+import { userRouter } from "../../routes/users.js";
+import { handleError, logger } from "../../middlewares/middlewares.js";
 const app = express();
 
 app.use(express.json());
+app.use("/users", userRouter);
+app.use(handleError, logger);
 
 const port = 3000;
 
-app.listen(port, (err) => {
-  if (err) {
-    console.error(err);
-  }
+app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
