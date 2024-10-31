2607ad544bb8f58c36a472f520d8d30c9c38e07e
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-10-31 14:45:13 -0300

    feat(middlewares.js): create authenticate function to auth token

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,3 +1,6 @@
+import jwt from "jsonwebtoken";
+const secretKey = "secret";
+
 const handleError = (err, req, res, next) => {
   const statusCode =
     err.status || (err.code && err.code.startsWith("SQLITE") ? 500 : 400);
@@ -12,6 +15,21 @@ const handleError = (err, req, res, next) => {
 const logger = (req, res, next) => {
   console.log(`Request method used: ${req.method}, URL used: ${req.url}`);
   next();
+};
+
+const authenticate = (req, res, next) => {
+  const header = req.headers["authorization"];
+  const token = header && header.split(" ")[1];
+
+  if (!token) return res.status(401).json({ message: "Token not provide" });
+
+  try {
+    const payload = jwt.verify(token, secretKey);
+    req.user = payload;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Invalid Token" });
+  }
 };
 
 export { handleError };
