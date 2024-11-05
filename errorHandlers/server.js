e1dcd3596f79dfa91e60e51b3255eede99555fd0
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-11-04 22:19:57 -0300

    fea: install morgan and winston to create a logger

--- errorHandlers/server.js
+++ errorHandlers/server.js
@@ -2,7 +2,14 @@ const serverHandleErrors = (err, req, res, next) => {
   if (err) {
     const errorMessage = err.message;
     const statusCode = err.statusCode;
-    console.error("Error stack:", err.stack);
+    const reqUrl = req.originalUrl;
+    const reqMethod = req.method;
+    const reqIP = req.ip;
+    logger.error(
+      `${
+        statusCode || 500
+      } - ${errorMessage} - ${reqUrl} - ${reqMethod} - ${reqIP}`
+    );
     res.status(statusCode).json({ error: errorMessage });
   }
   next();
--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userRouter from "../../routes/users.js";
 import dotenv from "dotenv";
+import morganMiddleWare from "../../logs/morgan.js";
 
 dotenv.config();
 const port = process.env.PORT;
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/users", userRouter);
+app.use(morganMiddleWare);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
