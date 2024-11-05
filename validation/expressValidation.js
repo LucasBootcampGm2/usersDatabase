a0e90172d85e3b5f2e0a78e4162b62e6c89d93ce
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-11-05 09:31:32 -0300

    feat(expressValidation.js): add more data sanitization

--- validation/expressValidation.js
+++ validation/expressValidation.js
@@ -5,8 +5,12 @@ const validateUser = [
     .isString()
     .isLength({ min: 2 })
     .withMessage("Name is required and should be at least 2 characters.")
+    .notEmpty()
+    .withMessage("Name cannot be empty.")
     .trim()
-    .escape(),
+    .escape()
+    .matches(/^[a-zA-Z\s]+$/)
+    .withMessage("Name should only contain letters and spaces."),
 
   body("email")
     .isEmail()
@@ -16,12 +20,20 @@ const validateUser = [
   body("password")
     .isLength({ min: 6 })
     .withMessage("Password must be at least 6 characters.")
-    .trim(),
+    .notEmpty()
+    .withMessage("Password cannot be empty.")
+    .trim()
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/)
+    .withMessage(
+      "Password must include uppercase, lowercase letters, and a number."
+    ),
 
   body("role")
     .optional()
     .isIn(["admin", "user"])
     .withMessage("Role must be either 'admin' or 'user'")
+    .notEmpty()
+    .withMessage("Role cannot be empty.")
     .trim()
     .escape(),
 ];
@@ -32,8 +44,15 @@ const validateParcialUpdate = [
     .isString()
     .isLength({ min: 2 })
     .withMessage("Name should be at least 2 characters.")
+    .notEmpty()
+    .withMessage("Name cannot be empty.")
     .trim()
-    .escape(),
+    .escape()
+    .matches(/^[a-zA-Z\s]+$/)
+    .withMessage(
+      "New password must include uppercase, lowercase letters, and a number."
+    )
+    .withMessage("Name should only contain letters and spaces."),
 
   body("email")
     .optional()
@@ -45,25 +64,32 @@ const validateParcialUpdate = [
 const validatePasswordChange = [
   body("oldPassword")
     .exists()
-    .withMessage("Old password is required")
+    .withMessage("Old password is required.")
     .isLength({ min: 6 })
-    .withMessage("Old password must be at least 6 characters long")
+    .withMessage("Old password must be at least 6 characters long.")
     .notEmpty()
-    .withMessage("Old password cannot be empty")
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/)
+    .withMessage("Old password cannot be empty.")
     .trim(),
 
   body("newPassword")
     .exists()
-    .withMessage("New password is required")
+    .withMessage("New password is required.")
     .isLength({ min: 6 })
-    .withMessage("New password must be at least 6 characters long")
+    .withMessage("New password must be at least 6 characters long.")
     .notEmpty()
-    .withMessage("New password cannot be empty")
+    .withMessage("New password cannot be empty.")
     .trim()
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/)
+    .withMessage(
+      "New password must include uppercase, lowercase letters, and a number."
+    )
     .custom((newPassword, { req }) => {
       const oldPassword = req.body.oldPassword;
       if (newPassword === oldPassword) {
-        throw new Error("New password must be different from the old password");
+        throw new Error(
+          "New password must be different from the old password."
+        );
       }
       return true;
     }),
@@ -77,7 +103,9 @@ const validateLogin = [
 
   body("password")
     .isLength({ min: 6 })
-    .withMessage("Password Length is not ok.")
+    .withMessage("Password length is not sufficient.")
+    .notEmpty()
+    .withMessage("Password cannot be empty.")
     .trim(),
 ];
 
